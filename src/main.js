999ebf1a0fed28b02c59bea08154e6e3a66782fa
20Aarya05 <aaryabhansalidab@gmail.com>
2025-01-19 03:40:38 +0530

    commit

--- src/main.js
+++ src/main.js
@@ -1,7 +1,6 @@
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
-import { lastClickedButton } from './caller.js';
 
 // Create the scene
 const scene = new THREE.Scene();
