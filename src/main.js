82060ed5f117fb66e86865d553b24ab0e27a8de5
20Aarya05 <aaryabhansalidab@gmail.com>
2025-01-19 03:36:38 +0530

    commit

--- src/main.js
+++ src/main.js
@@ -56,10 +56,6 @@ spotLight.distance = 100;
 spotLight.castShadow = true;
 scene.add(spotLight);
 
-// Hemisphere Light
-const hemisphereLight = new THREE.HemisphereLight(0xddeeff, 0x0f0e0d, 0.3);  // Reduced intensity
-scene.add(hemisphereLight);
-
 const directionalLight = new THREE.DirectionalLight(0xffffff, 2); // Bright white light
 directionalLight.position.set(5, 5, 5); // Position of the light
 scene.add(directionalLight);
