f7c30186281332a6056a870d2bae5ddfed4a1730
20Aarya05 <aaryabhansalidab@gmail.com>
2025-01-19 03:38:49 +0530

    commit

--- src/main.js
+++ src/main.js
@@ -32,21 +32,6 @@ window.addEventListener('resize', onWindowResize);
 const ambientLight = new THREE.AmbientLight(0xffffff, 0.5);  // Reduced intensity
 scene.add(ambientLight);
 
-// Directional Light with Shadows
-const directionalLight = new THREE.DirectionalLight(0xffffff, 0.5);  // Reduced intensity
-directionalLight.position.set(10, 20, 10);
-directionalLight.castShadow = true;
-directionalLight.shadow.mapSize.width = 2048; // High resolution shadow map
-directionalLight.shadow.mapSize.height = 2048;
-directionalLight.shadow.camera.near = 1;
-directionalLight.shadow.camera.far = 100;
-directionalLight.shadow.camera.left = -20;
-directionalLight.shadow.camera.right = 20;
-directionalLight.shadow.camera.top = 20;
-directionalLight.shadow.camera.bottom = -20;
-scene.add(directionalLight);
-
-
 // Load a GLTF model
 const loader = new GLTFLoader();
 const modelPath = '/assets/models/Typical_LiftLobby.glb'; // Path to the model
