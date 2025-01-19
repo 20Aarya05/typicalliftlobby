6d7d8d082e43ead3a9ebaa18fa20c657a289425f
20Aarya05 <aaryabhansalidab@gmail.com>
2025-01-19 11:46:39 +0530

    commit

--- vite.config.js
+++ vite.config.js
@@ -7,26 +7,12 @@ export default defineConfig({
   },
   build: {
     outDir: 'dist', // Output directory for the build
-    chunkSizeWarningLimit: 1000, // Increase the chunk size limit to 1 MB (1000 KB)
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          // Split react and react-dom into a vendor chunk
-          if (id.includes('node_modules/react') || id.includes('node_modules/react-dom')) {
-            return 'vendor'; // Bundle react and react-dom into a single vendor chunk
-          }
-          // Optionally, you can also split other libraries or files here
-          if (id.includes('src/utils')) {
-            return 'utils'; // Split custom utils into a separate chunk
-          }
-        }
-      }
-    }
+    chunkSizeWarningLimit: 1000 // Increase the chunk size limit to 1 MB (1000 KB)
   },
   resolve: {
     alias: {
       '@': '/src' // Optional: Short path for importing files
     }
   },
-  publicDir: 'src',
+  publicDir: 'src'
 });
