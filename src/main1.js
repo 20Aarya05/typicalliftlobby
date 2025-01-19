20c7821b927235d8599759d52a00c61749ae77a0
20Aarya05 <aaryabhansalidab@gmail.com>
2025-01-19 12:19:43 +0530

    commit

--- src/main1.js
+++ src/main1.js
@@ -43,35 +43,6 @@ window.addEventListener('resize', onWindowResize);
 const ambientLight = new THREE.AmbientLight(0xffffff, 0.5);  // Reduced intensity
 scene.add(ambientLight);
 
-// Directional Light with Shadows
-const directionalLight = new THREE.DirectionalLight(0xffffff, 0.5);  // Reduced intensity
-directionalLight.position.set(10, 20, 10);
-directionalLight.castShadow = true;
-directionalLight.shadow.mapSize.width = 2048; // High resolution shadow map
-directionalLight.shadow.mapSize.height = 2048;
-directionalLight.shadow.camera.near = 1;
-directionalLight.shadow.camera.far = 100;
-directionalLight.shadow.camera.left = -20;
-directionalLight.shadow.camera.right = 20;
-directionalLight.shadow.camera.top = 20;
-directionalLight.shadow.camera.bottom = -20;
-scene.add(directionalLight);
-
-// Spotlight
-const spotLight = new THREE.SpotLight(0xffa95c, 1);  // Reduced intensity
-spotLight.position.set(-15, 30, 15);
-spotLight.angle = Math.PI / 4;
-spotLight.penumbra = 0.2;
-spotLight.decay = 2;
-spotLight.distance = 100;
-spotLight.castShadow = true;
-scene.add(spotLight);
-
-// Hemisphere Light
-const hemisphereLight = new THREE.HemisphereLight(0xddeeff, 0x0f0e0d, 0.3);  // Reduced intensity
-scene.add(hemisphereLight);
-
-
 // Load the textures
 const textureLoader = new THREE.TextureLoader();
 
